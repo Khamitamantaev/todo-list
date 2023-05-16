b1a42dbddf08b64e06b04727f49ba24b29674e8b
khamitamantaev@gmail.com <khamitamantaev@gmail.com>
2023-05-16 22:22:06 +0600

    simple dep init and test app run
    Working correctly

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const plugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'none',
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    target: 'web',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new plugin({
+            template: path.resolve( __dirname, 'public/index.html' ),
+            filename: 'index.html'
+        })
+    ]
+}
